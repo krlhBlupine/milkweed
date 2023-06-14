1b101d6aafb6940760cafd523f2220c39aeeb9d3
KRLH Blupine <krlh.blupine@proton.me>
2023-06-13 19:12:18 -0500

    changed colors and meddled with the js

--- script.js
+++ script.js
@@ -8,48 +8,48 @@ const toggle_c = document.querySelector("#toggle_c");
 const toggle_d = document.querySelector("#toggle_d");
 
 // TOGGLES FOR SLIDER A
-toggle_a.addEventListener("mouseover", function() {
+toggle_a.addEventListener("click", function() {
     let isOpen = slider_a.classList.contains("slide-in");
 
     slider_a.setAttribute("class", "slide-out");
 });
-toggle_a.addEventListener("mouseout", function() {
+slider_a.addEventListener("mouseleave", function() {
     let isOpen = slider_a.classList.contains("slide-out");
 
     slider_a.setAttribute("class", "slide-in");
 });
 
 // SLIDER B
-toggle_b.addEventListener("mouseover", function() {
+toggle_b.addEventListener("click", function() {
     let isOpen = slider_a.classList.contains("slide-in");
 
     slider_b.setAttribute("class", "slide-out");
 });
-toggle_b.addEventListener("mouseout", function() {
+slider_b.addEventListener("mouseleave", function() {
     let isOpen = slider_a.classList.contains("slide-out");
 
     slider_b.setAttribute("class", "slide-in");
 });
 
 // SLIDER C
-toggle_c.addEventListener("mouseover", function() {
+toggle_c.addEventListener("click", function() {
     let isOpen = slider_a.classList.contains("slide-in");
 
     slider_c.setAttribute("class", "slide-out");
 });
-toggle_c.addEventListener("mouseout", function() {
+slider_c.addEventListener("mouseleave", function() {
     let isOpen = slider_a.classList.contains("slide-out");
 
     slider_c.setAttribute("class", "slide-in");
 });
 
 // SLIDER D
-toggle_d.addEventListener("mouseover", function() {
+toggle_d.addEventListener("click", function() {
     let isOpen = slider_d.classList.contains("slide-in");
 
     slider_d.setAttribute("class", "slide-out");
 });
-toggle_d.addEventListener("mouseout", function() {
+slider_d.addEventListener("mouseleave", function() {
     let isOpen = slider_d.classList.contains("slide-out");
 
     slider_d.setAttribute("class", "slide-in");
